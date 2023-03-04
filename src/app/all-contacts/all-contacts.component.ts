918f0c4f215e04bd7e12deb110e41ecf1ee76dcc
soniagkalliath <soniageorge111@gmail.com>
2023-03-04 14:40:10 +0530

    contact management app with validation completed

---
+++ src/app/all-contacts/all-contacts.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-all-contacts',
+  templateUrl: './all-contacts.component.html',
+  styleUrls: ['./all-contacts.component.css']
+})
+export class AllContactsComponent implements OnInit {
+  // to hold all contacts data from api
+  allContacts:any = []
+  loginDetails:any ;
+  searchKey:string =''
+  constructor(private api:ApiService){
+    this.loginDetails = new Date()
+  }
+  ngOnInit(): void {
+    //to get all conatcts
+    this.getAllContact()
+  }
+
+  //to get all conatcts
+  getAllContact(){
+    // api call to get all contacts
+    this.api.allContacts()
+    .subscribe((result:any)=>{
+      //result is all contacts array  from api
+      console.log(result);
+      this.allContacts = result
+    })
+  }
+  
+  //deleteContact
+  deleteContact(contactId:any){
+    this.api.deleteContact(contactId)
+    .subscribe((data:any)=>{
+   //  window.location.reload()
+   this.getAllContact()
+    })
+  }
+}
